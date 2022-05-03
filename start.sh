ec6e8dd7cf1da193b7ed99beceb46327b78a617b
simpleboykrishna0 <44639656+simpleboykrishna0@users.noreply.github.com>
2022-05-03 15:45:31 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,7 @@
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt install xfce4 xfce4-goodies -y
+sudo apt install dbus-x11 -y
+sudo apt install tigervnc-standalone-server -y
+npm install -g localtunnel
+vncpasswd
