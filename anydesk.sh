2aaec4c239d6bf6189d1d8560bb0b4b348d7220d
simpleboykrishna0 <44639656+simpleboykrishna0@users.noreply.github.com>
2022-05-03 16:35:11 +0530

    Create anydesk.sh

---
+++ anydesk.sh
@@ -0,0 +1,4 @@
+wget -qO - https://keys.anydesk.com/repos/DEB-GPG-KEY | apt-key add -
+echo "deb http://deb.anydesk.com/ all main" > /etc/apt/sources.list.d/anydesk.list
+sudo apt update
+sudo apt install anydesk
